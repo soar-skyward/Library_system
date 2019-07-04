7bf4e4196b73402c2927746733740bd8c9798e35
LiuHan's Laptop <liuhan_1631@163.com>
2019-07-04 17:44:50 +0800

    补全上次push遗漏的文件

--- borrower.h
+++ borrower.h
@@ -1,11 +1,6 @@
 #ifndef BORROWER
 #define BORROWER
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <malloc.h>
-#include <io.h>
 #include "public.h"
 
 struct account_Borrower
